fe9a8c9ebdbb520a80734ff1e09c2d3981092e6b
xiake1111 <695660735@qq.com>
2024-04-13 10:22:06 +0800

    画板中文版

---
+++ __init__.py
@@ -0,0 +1,9 @@
+
+from .PainterNode.painter_node import PainterNodeZWB
+NODE_CLASS_MAPPINGS = {
+    "PainterNodeZWB" : PainterNodeZWB,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "PainterNodeZWB" : "2🐕AlekPet画板中文版",
+}
